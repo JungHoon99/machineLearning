1b619c480fb4a89cea8f9331fa9b4afff45725f8
JungHoon99 <44799091+JungHoon99@users.noreply.github.com>
2019-02-09 22:18:39 +0900

    Update 딥러닝 날씨 QNA 챗봇.py

--- 딥러닝 날씨 QNA 챗봇.py
+++ 딥러닝 날씨 QNA 챗봇.py
@@ -2,18 +2,17 @@
 from konlpy.tag import Twitter
 import numpy as np
 
+#필요한 변수
 check=None
 k=Twitter()
 senten=[]
 sentence=[]
 dic={}
 
-
-#웹 크롤링 객체
+#bs4를 이용한 웹크롤링
 from urllib.request import urlopen, Request
 import urllib
 import bs4
-import time
 
 location = ''
 enc_location = urllib.parse.quote(location + '+날씨')
@@ -26,6 +25,8 @@ html = page.read()
 type(html)
 soup = bs4.BeautifulSoup(html,'html5lib')
 
+
+#날씨 웹크롤링 객체화
 class weather():
     def __init__(self):
         self.today=0
@@ -40,37 +41,42 @@ class weather():
         self.high_d=soup.find('ul', class_='info_list').find('span', class_='max').find('span', class_='num').text
         self.low_d=soup.find('ul', class_='info_list').find('span', class_='min').find('span', class_='num').text
         self.cast=soup.find('ul', class_='info_list').find('p', class_='cast_txt').text
+        self.dust=(str(soup.find('dl', class_='indicator').text).split()[1][:len(str(soup.find('dl', class_='indicator').text).split()[1])-2],str(soup.find('dl', class_='indicator').text).split()[1][-2:])
 
-    def get_degree(self):
+    def get_degree(self):   #현재 온도 반환
         return self.degree
 
-    def get_high_d(self):
+    def get_high_d(self):   #오늘의 최고기온
         return self.high_d
 
-    def get_low_d(self):
+    def get_low_d(self):    #오늘의 최저기온
         return self.low_d
 
-    def get_cast(self):
+    def get_cast(self):     #현재 날씨 상황
         return self.cast
 
-#데이터 학습을 위 한 전처리 함수
-def vectorize_sequences(sequences,diemension=33154):
+    def get_dust(self):     #미세먼지 지수
+        return self.dust
+
+# one_hot 벡터화 함수
+def vectorize_sequences(sequences,diemension=33182):
     result=np.zeros((len(sequences),diemension))
     for i,sequence in enumerate(sequences):
         result[i,sequence]=1.
     return result
 
-#파일 불러오기
+#파일 읽어오기
 with open('words2.txt','rt') as f:
     text=f.read()
 
-#파싱 하기
+#뛰어쓰기 대로 파싱
 text=text.split()
 
 #형태소 분석
 for i in range(len(text)):
     senten.extend(k.pos(text[i]))
 
+#단어 분리
 for i in range(len(senten)):
     if((senten[i][0] in sentence)==False):
         sentence.append(senten[i][0])
@@ -82,20 +88,23 @@ for i in range(len(sentence)):
         dic.update({sentence[i]:a})
         a+=1
 
-#훈련에 사용 되는 문장들 형재 날씨와 최고기온 최저기온 미세먼지상태를 질문 가능하게 함
+#학습 문장 데이터
 senten=["오늘은 날씨는 어때","오늘 날씨 좀 알려줘",
 "오늘 날씨 어때","오늘 날씨 좋아","오늘은 날씨가 어때",
 "안녕","안녕하세요","오늘 최고기온이 몇도야",
 "오늘 최고기온","최고기온은","최고기온","오늘 최저기온이 몇도야",
-"오늘 최저기온","최저기온은","최저기온","미세먼지 어때?","미세먼지 알려줘","미세먼지 안좋아?"]
+"오늘 최저기온","최저기온은","최저기온","미세먼지 어때?","미세먼지 알려줘",
+"미세먼지 안좋아?","오늘 어때","오늘 좋을까 안좋을까?","오늘 안좋아?"]
 
 x_train=[]
-#매칭되는 결과값들 one-hot 으로 표현
+# 학습 문장에 맞는 결과 데이터
 y_train=[[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0],
          [0,1,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],[0,0,1,0,0],
-         [0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,0,1],[0,0,0,0,1],[0,0,0,0,1]]
+         [0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,1,0],[0,0,0,0,1],[0,0,0,0,1],
+         [0,0,0,0,1],[1,0,0,0,0],[1,0,0,0,0],[1,0,0,0,0]]
 
 
+#학습 문장 과 결과 데이터 가공
 for j in range(len(senten)):
     test=k.pos(senten[j])
     data=[]
@@ -107,19 +116,25 @@ x_train=vectorize_sequences(x_train)
 
 y_train=np.array(y_train)
 
+#딥러닝 학습 모듈
 from keras import models
 from keras import layers
 from keras import optimizers
 
+#모델 설정
 model = models.Sequential()
-model.add(layers.Dense(16,activation='relu',input_shape=(33154,)))
+#총 3개의 은닉층을 가진 모델 층 쌓기  
+model.add(layers.Dense(16,activation='relu',input_shape=(33182,)))
 model.add(layers.Dense(16,activation='softmax'))
 model.add(layers.Dense(5,activation='softmax'))
 
+#모델의 오티마이저 설정과 손실 함수 설정
 model.compile(optimizer=optimizers.RMSprop(lr=0.001),loss='categorical_crossentropy',metrics=['acc'])
 
-model.fit(x_train,y_train,batch_size=18 ,epochs=2500)
+#모델 학습
+model.fit(x_train,y_train,batch_size=21 ,epochs=2500)
 
+#학습 신경망에서의 결과 출력
 def request():
     senten=k.pos(ent.get())
     x_test=[]
@@ -129,27 +144,29 @@ def request():
     x_test.append(list(data))
     x_test=vectorize_sequences(x_test)
     predict = model.predict(x_test).argmax(axis=1)
-    lab['text']=predict
-    
+    action(predict)
+
+#대답
 def action(kind):
     W=weather()
     W.crawling()
     if(kind==0):
-        lab['text']=str(W.get_cast())
+        lab['text']="날씨는 "+str(W.get_cast())+" (현재온도 : "+ str(W.get_degree())+")"
     elif(kind==1):
         lab['text']="안녕하세요"
     elif(kind==2):
-        lab['text']=str(W.get_high_d())
+        lab['text']="최고기온은 "+str(W.get_high_d())+"도 입니다."
     elif(kind==3):
-        lab['text']=str(W.get_low_d())
+        lab['text']="최저기온은 "+str(W.get_low_d())+"도 입니다."
     elif(kind==4):
-        lab['text']="미세먼지는 안좋아요"
+        a=W.get_dust()
+        lab['text']="미세먼지는 "+a[0]+"로 "+a[1]+"이에요"
 
 from tkinter import *
 
 main=Tk()
 
-ent=Entry(main,width=100)
+ent=Entry(main,width=70)
 ent.grid()
 but=Button(main,text='확인',command=request)
 but.grid(row=0,column=1)
