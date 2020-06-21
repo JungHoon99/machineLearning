23ada7e6171699837961c98902302fcc087985c9
정훈 <greenjm20214@gmail.com>
2020-06-21 07:39:28 +0000

    tensorflow1 사용법

--- xorMuchine.py
+++ xorMuchine.py
@@ -2,27 +2,27 @@ from tensorflow import keras
 import numpy as np
 import time
 
-x_data=[[0,0],[0,1],[1,0],[1,1]]
-y_data=[[0],[1],[1],[0]]
+x_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+y_data = [[0], [1], [1], [0]]
 
-x_data=np.array(x_data)
-y_data=np.array(y_data)
+x_data = np.array(x_data)
+y_data = np.array(y_data)
 
-model=keras.Sequential()
+model = keras.Sequential()
 
-model.add(keras.layers.Dense(8,activation="sigmoid",input_shape=(2,)))
-model.add(keras.layers.Dense(1,activation="sigmoid"))
+model.add(keras.layers.Dense(8, activation="sigmoid", input_shape=(2,)))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
 
-optimizer=keras.optimizers.SGD(lr=0.1)
+optimizer = keras.optimizers.SGD(lr=0.1)
 
-model.compile(optimizer=optimizer,loss="binary_crossentropy",metrics=['accuracy'])
+model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=['accuracy'])
 model.summary()
 
-model.fit(x_data,y_data,batch_size=3,epochs=2000)
+model.fit(x_data, y_data, batch_size=3, epochs=2000)
 
-test=[[0,0],[1,1]]
-test=np.array(test)
+test = [[0, 0], [1, 1]]
+test = np.array(test)
 
-predict=model.predict(x_data)
+predict = model.predict(x_data)
 print(np.round(predict))
 time.sleep(5)
